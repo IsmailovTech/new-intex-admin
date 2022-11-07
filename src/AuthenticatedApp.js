edb400f9d277685a9da6c38e4cde8d283fd4fa64
kholiqulov_nodirbek <99204049+Xoliqulov-Nodirbek@users.noreply.github.com>
2022-11-07 15:35:09 +0500

    Qwerty

--- src/AuthenticatedApp.js
+++ src/AuthenticatedApp.js
@@ -8,7 +8,7 @@ import Products from "./Pages/Products/Products";
 
 function AuthenticatedApp() {
     console.log('hello world')
-    console.log('goodbye world')
+    console.log('hay world')
     return (
         <div className="conatainer flex">
             <Navigation />
