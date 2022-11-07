f8e210edccff3f9873daa6c8da3482c3c99bf88d
Ibrohimbek <ismailovibrokhim98@gmail.com>
2022-11-07 14:52:13 +0500

    changed

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -6,6 +6,7 @@ const Navigation = () => {
       <nav className='nav'>
         <ul className='nav__list'>
             <li className='nav__item'>
+              <h1>news</h1>
                 <Link to='/'>Home</Link>
                 <Link to='/products'>Products</Link>
             </li>
